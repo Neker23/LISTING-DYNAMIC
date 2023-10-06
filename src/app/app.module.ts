2ae797ad7754a7ceede6517ccdcdf7eb5070c4ea
Neker23 <nekersanchez9@gmail.com>
2023-10-06 17:30:17 -0500

    boton add agregado, librería flag-icon agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 
-
 @NgModule({
   declarations: [
     AppComponent,
